8fcf56eb8632f74a7b669f1367790e2ce4a9650d
choba <marcelo.alckmin22@gmail.com>
2024-03-17 17:36:31 -0300

    terminado

--- codigos_teoria/ex_avaliativo.py
+++ codigos_teoria/ex_avaliativo.py
@@ -7,29 +7,29 @@ client = MongoClient("mongodb://localhost:27017")
 db = client['bancoiot']
 sensores = db.sensores
 
-
 def random_temp(nome):
     while True:
-        temp = random.randint(30,40) #gera os valores de temperatura aleatórios
-        print(f"{temp}ºC")
-        sensores.update_one(
-            {'nomeSensor': nome},
-            {'$Set'{'valorSensor': temp}}
-            )  
-        if temp > 38:
+        sensorAtivado = sensores.find_one({"nomeSensor": nome})
+        if not sensorAtivado["sensorAlarmado"]:
+            temp = random.randint(30,40) #gera os valores de temperatura aleatórios
+            print(f"{temp}ºC")
             sensores.update_one(
                 {'nomeSensor': nome},
-                {'$set':{'sensorAlarmado': True}}
+                {'$set':{'valorSensor': temp}}
             )
-            print(f"Atenção! Temperatura muito alta! Verificar sensor {nome}!")
-            break
-        time.sleep(10)
-      
+            if temp > 38:
+                sensores.update_one(
+                    {'nomeSensor': nome},
+                    {'$set':{'sensorAlarmado': True}}
+                )
+                print(f"Atenção! Temperatura muito alta! Verificar sensor {nome}!")
+                break
+            time.sleep(5)
 
 
-sensor1 = threading.Thread(target= random_temp, args=" Temp 1")
-sensor2 = threading.Thread(target= random_temp, args=" Temp 2")
-sensor3 = threading.Thread(target= random_temp, args= "Temp 3")
+sensor1 = threading.Thread(target=random_temp, args=("Temp 1",))
+sensor2 = threading.Thread(target=random_temp, args=("Temp 2",))
+sensor3 = threading.Thread(target=random_temp, args=("Temp 3",))
 
 sensor1.start()
 sensor2.start()
